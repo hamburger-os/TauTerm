309ad2b7087099e3d44c56d9f2818ee8ad750894
hamburger-os <296570182@qq.com>
2026-09-03 22:10:07 +0800

    feat(trdp): register frontend plugin

---
+++ src/plugins/trdp/index.tsx
@@ -0,0 +1,21 @@
+import { registerPlugin } from "../../core/plugin-registry";
+import TrdpConnectForm from "./TrdpConnectForm";
+import TrdpSessionView from "./TrdpSessionView";
+
+registerPlugin({
+  manifest: {
+    id: "trdp",
+    name: "TRDP",
+    version: "1.0.0",
+    category: "network_tool",
+    description: "TRDP · Train Real Time Data Protocol",
+    icon: "globe",
+    content_type: "custom",
+    capabilities: ["connection", "network_outbound", "network_listen"],
+    transfer_protocols: [],
+  },
+  connectForm: TrdpConnectForm,
+  customView: TrdpSessionView,
+});
+
+console.log("[Plugin] TRDP plugin registered");
