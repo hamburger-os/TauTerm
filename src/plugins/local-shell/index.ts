ae8d4fa5988bb4df269b967ad32eb57c8c77d6f7
hamburger-os <296570182@qq.com>
2026-09-08 00:16:12 +0800

    refactor: consume canonical plugin manifest

--- src/plugins/local-shell/index.ts
+++ src/plugins/local-shell/index.ts
@@ -1,19 +1,9 @@
-import { registerPlugin } from "../../core/plugin-registry";
+import { registerPlugin, type PluginManifest } from "../../core/plugin-registry";
+import manifestJson from "../../plugin-manifests/local-shell.json";
 import LocalShellConnectForm from "./LocalShellConnectForm";
 
 registerPlugin({
-  manifest: {
-    id: "local-shell",
-    name: "Local Shell",
-    version: "1.0.0",
-    category: "terminal",
-    description: "Local Shell",
-    icon: "ssh-shell",
-    content_type: "terminal",
-    send_bar: false,
-    capabilities: ["connection", "endpoint_discovery", "multi_session", "elevated_session", "session_logging"],
-    transfer_protocols: [],
-  },
+  manifest: manifestJson as PluginManifest,
   connectForm: LocalShellConnectForm,
   toolbarItems: [],
 });
