ef132f5e1229110babc91ed35fc2d522a140a93b
hamburger-os <296570182@qq.com>
2026-09-09 14:36:06 +0800

    fix: keep direct TypeScript tool imports Node-resolvable

--- src/utils/encoding.ts
+++ src/utils/encoding.ts
@@ -1,5 +1,5 @@
-import { bytesToHex, parseByteInput } from "./byteInput";
-import { toolErr, toolOk, type ToolResult } from "./toolResult";
+import { bytesToHex, parseByteInput } from "./byteInput.ts";
+import { toolErr, toolOk, type ToolResult } from "./toolResult.ts";
 
 export interface EncodingOptions {
   base64IgnoreWhitespace?: boolean;
