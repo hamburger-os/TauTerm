332fca76c1b3d25c49e34cfc9151d79f3cc01dfc
hamburger-os <296570182@qq.com>
2026-09-11 11:57:23 +0800

    format virtual port module declarations

--- src-tauri/src/virtual_port/mod.rs
+++ src-tauri/src/virtual_port/mod.rs
@@ -13,7 +13,7 @@ pub mod backend;
 pub mod bridge;
 #[cfg(target_os = "windows")]
 pub mod manager;
-#[cfg(target_os = "windows")]
-pub mod service_backend;
 #[cfg(not(target_os = "windows"))]
 pub mod pty;
+#[cfg(target_os = "windows")]
+pub mod service_backend;
