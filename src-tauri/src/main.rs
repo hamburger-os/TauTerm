262ca8fd40d331ad3ee7d127ccdbc2d7248b42c0
hamburger-os <296570182@qq.com>
2026-09-18 13:37:16 +0800

    refactor(vport): route privileged helper before Tauri startup

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,7 +4,7 @@
 
 fn main() {
     #[cfg(windows)]
-    if tauterm_lib::maybe_run_elevated_shell_helper() {
+    if tauterm_lib::maybe_run_elevated_helper() {
         return;
     }
     tauterm_lib::run();
