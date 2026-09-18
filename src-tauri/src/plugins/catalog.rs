c0ed0057f76f3649863987e391db1fee4e2c42c1
hamburger-os <296570182@qq.com>
2026-09-18 10:10:16 +0800

    feat: register RTT automation target command

--- src-tauri/src/plugins/catalog.rs
+++ src-tauri/src/plugins/catalog.rs
@@ -47,6 +47,7 @@ macro_rules! tauterm_invoke_handler {
             $crate::plugins::rtt::commands::rtt_history,
             $crate::plugins::rtt::commands::rtt_write,
             $crate::plugins::rtt::commands::rtt_refresh_channels,
+            $crate::plugins::rtt::commands::rtt_set_automation_channel,
             $crate::plugins::local_shell::resolve_local_shell_session_name,
             $crate::plugins::ssh::commands::sftp_list_dir_cmd,
             $crate::plugins::ssh::commands::sftp_stat_cmd,
