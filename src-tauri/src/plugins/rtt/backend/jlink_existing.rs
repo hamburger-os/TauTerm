a36eaef6adc6805a31de8c240ef14d81ef724f23
hamburger-os <296570182@qq.com>
2026-09-20 19:18:31 +0800

    Mark J-Link RTT directions usable

--- src-tauri/src/plugins/rtt/backend/jlink_existing.rs
+++ src-tauri/src/plugins/rtt/backend/jlink_existing.rs
@@ -63,8 +63,8 @@ impl JlinkExistingRttBackend {
             .map(|index| RttChannelInfo {
                 index: *index,
                 name: None,
-                up: Some(RttChannelDirectionInfo { buffer_size: None }),
-                down: Some(RttChannelDirectionInfo { buffer_size: None }),
+                up: Some(RttChannelDirectionInfo { buffer_size: None, usable: true, issue: None }),
+                down: Some(RttChannelDirectionInfo { buffer_size: None, usable: true, issue: None }),
                 metadata_complete: false,
             })
             .collect();
