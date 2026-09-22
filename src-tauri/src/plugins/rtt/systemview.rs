5cec89c5d0ba509dc0bfa13fc44509068a555c36
hamburger-os <296570182@qq.com>
2026-09-22 18:42:42 +0800

    style(rtt): apply rustfmt to target loss rate

--- src-tauri/src/plugins/rtt/systemview.rs
+++ src-tauri/src/plugins/rtt/systemview.rs
@@ -496,8 +496,8 @@ impl TraceState {
             .iter()
             .filter(|(at, _)| now.saturating_duration_since(*at) <= TARGET_DROP_RATE_WINDOW)
             .fold(0u64, |sum, (_, dropped)| sum.saturating_add(*dropped));
-        let target_drop_rate = (u128::from(recent_target_drops) * 1_000)
-            .div_ceil(TARGET_DROP_RATE_WINDOW.as_millis());
+        let target_drop_rate =
+            (u128::from(recent_target_drops) * 1_000).div_ceil(TARGET_DROP_RATE_WINDOW.as_millis());
         let target_drop_rate_per_sec = u64::try_from(target_drop_rate).unwrap_or(u64::MAX);
 
         let mut tasks = self
